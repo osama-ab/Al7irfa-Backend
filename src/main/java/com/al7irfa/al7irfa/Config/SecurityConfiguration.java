80eb855fd234ea74c1bd176a950d81a4892f144b
mouad <emouad352@gmail.com>
2023-08-24 14:20:06 +0100

    08-24-MoUAD

--- src/main/java/com/al7irfa/al7irfa/Config/SecurityConfiguration.java
+++ src/main/java/com/al7irfa/al7irfa/Config/SecurityConfiguration.java
@@ -26,7 +26,7 @@ public class SecurityConfiguration {
                 csrf().
                 disable()
                 .authorizeHttpRequests()
-                .requestMatchers("/api/auth/**","/users/**")
+                .requestMatchers("**")
                 .permitAll()
                 .requestMatchers(HttpMethod.OPTIONS,"/**")
                 .permitAll()
